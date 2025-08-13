8364284c887a7786b89af5fa64a1e3eea58f7b83
Siddhartha Sarkar <siddhartha.sarkar@lab49.com>
2025-08-13 15:53:43 +0530

    bootstrap with astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'EpochWise';
+export const SITE_DESCRIPTION = 'This is my "Today I Learned" style blog on DevSecOps, MLOps, and AI engineering — capturing lessons, experiments, and learning paths one epoch at a time.';
